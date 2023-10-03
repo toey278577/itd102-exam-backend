9e6c24c168f688561319e217c3d05b1339baeece
toey278577 <sutad27857@gmail.com>
2023-10-03 18:10:45 +0700

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 var corsOptions = {
-    origin: "http://localhost:3001"
+    origin: "http://localhost:3000"
 }
 
 app.use(cors(corsOptions));
